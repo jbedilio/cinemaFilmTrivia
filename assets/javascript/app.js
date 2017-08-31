978e17ba75e40690b075cd3a2032d185971e8471
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-08-31 10:14:50 -0400

    completed minimum viable product

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,31 +7,16 @@ $(document).ready(function () {
 
         $('#startUp').append($start);
         $($start).on('click', function() {
-                $('#title, #clock, #questions, #submit-btn, #right, #wrong').show();
+                $('#title, #clock, #questions, #submit-btn').show();
                 $('#startUp').hide();
         });
-        setTimeout(function(){
-                $('#title, #clock, #questions, #submit-btn, #right, #wrong').hide();
-        }, 1000 * 60);
+        /*setTimeout(function(){
+                $('#title, #clock, #questions, #submit-btn').hide();
+                $('#right, #wrong').show();
+        }, 1000 * 4);*/
         
         var $right = 0;
         var $wrong = 0;
-
-        /*var $triviaQA = {
-                
-                        Q: [          "Who starred in both The Matrix and The Lord of the Rings trilogies?",
-                                      "Who starred in the Quentin Tarantino film Reservoir Dogs as Mr. Yellow?",
-                                      
-                                
-                        ],
-
-
-                        A: [        "Tom Cruise", "Liv Tyler", "Sean Bean", "Hugo Weaving", "Keanu Reeves", "Arnold                             Schwarzenegger","Sylvester Stalone", "Nicole Kidman", "Michael Madsen", "Steve                             Buscemi", "Jennifer Connelly", "Matt Damon", "Leonardo Dicaprio", "Tom Hanks",                             "Harrison Ford", "Morgan Freeman", "Adam Sandler","Woody Harrelson", "Bruce                                Willis", "Robin Williams", "Jesse Ventura", "Emma Watson", "Julia Roberts",                                "Cameron Diaz", "Amanda Peete", "Scarlett Johansson", "Stan Lee", "Chris Pratt"
-                        ]
-                        };*/
-
-
-        /*var $answersF = [       "Armageddon", "Enemy of the State", "Terminator", "Predator", "Deep Impact", "Good Will Hunting",                           "Titanic ", "Boogie Nights ", "Donnie Brasco", "The Rainmaker"]*/
 
         var triviaQA = [{
                         question: "Who starred in both The Matrix and The Lord of the Rings trilogies?",
@@ -51,18 +36,28 @@ $(document).ready(function () {
                         correct:  "Titanic",
                 }
         ];
-        
-        //for(var i = 0; i < triviaQA.length; i++) {
-        $('#questions').html(triviaQA[0].question);
+        //Minimum Viable Product
+        $('#questions').html(triviaQA[0].question + '<br>');
         var $form = $("<form>");
         $("#questions").append($form);
 
-        for(var j = 0; j < triviaQA[0].answer.length; j++){
-        var $radioBtn = $("<input type='radio' name='q1' value='" + triviaQA[0].answer[j] + "'>" + triviaQA[0].answer[j]+ "</input>");
-        $($radioBtn).appendTo($form);
+        for (var j = 0; j < triviaQA[0].answer.length; j++) {
+                var $radioBtn = $("<input type='radio' name='q1' value='" + triviaQA[0].answer[j] + "'>" + triviaQA[0].answer[j] + "</input><br>");
+                $($radioBtn).appendTo($form);
 
         };
+        
+        //my working loop that loops each questions with the possible answers with radio buttons -- it works but my grader at end of code doesn't work
 
+        /*for(var i = 0; i < triviaQA.length; i++) {
+        var $form = $("<form>");
+        $('#questions').append(triviaQA[i].question + '<br>');
+        for (var j = 0; j < triviaQA[i].answer.length; j++) {
+                var $radioBtn = $("<input type='radio' name='q" + [i] +"' value='" + triviaQA[i].answer[j] + "'> " + triviaQA[i].answer[j] + "</input><br>");
+                $($radioBtn).appendTo($form);
+        $("#questions").append($form);
+        };
+        };*/
         var $submit = ('#submit-btn');
         
         var $button2 = $('<input name="submit" type="button" value="submit">');
@@ -70,70 +65,63 @@ $(document).ready(function () {
         $($submit).append($button2);
 
         $($submit).on('click', function grade() {
-        var a1 = document.getElementsByName('q1')
-          for (var i = 0; i < a1.length; i++) {
-        if (a1[i].checked) {
-                if (a1[i].value == "Hugo Weaving") {
-                $right++;
-                $('#right').html('Correct: ' + $right);
-        } else {
-                $wrong++
-                $('#wrong').html('Incorrect: ' + $wrong);
+                $('#title, #clock, #questions, #submit-btn').hide()
+                $('#right, #wrong').show();
+                var a1 = document.getElementsByName('q1')
+                for (var i = 0; i < a1.length; i++) {
+                        if (a1[i].checked) {
+                                if (a1[i].value == "Hugo Weaving") {
+                                        $right++;
+                                        $('#right').html('Correct: ' + $right);
+                                } else {
+                                        $wrong++
+                                        $('#wrong').html('Incorrect: ' + $wrong);
+                                };
+                        };
                 };
-        };
-        };
         });
-/*function timeUp(){
-        function grade()
-};*/
-
-        // var $QADiv = $('#questions')
-        // for (var i = 0; i < $triviaQA.length; i++) {
-        //         for (var j = 0; j < $triviaQA[i].answer.length; j++) {
-        //                 console.log('Question', $triviaQA[i].length, $triviaQA[i].answer);
-        //                 var $input = $('<div><br><input type="radio" name="a" value="Hugo Weaving"> ' + $triviaQA[i].answer + '</div>');
-        //                 console.log($input);
-        //                 console.log($triviaQA[i].q);
-        //                 //if ($triviaQA[j].q) {}
-        //                 //$QADiv.html($triviaQA[j].q).append($input);
-        //                 //console.log($QAdiv);
-
-        //                 //if ($triviaQA[i][j].q))
-        //                 /*+ '<br>' + quest2.a + '<br><input type="radio" name="b" value="Predator"> ' + quest2.a[j] + '<br>' +
-        //                         quest3.qa() + '<br><input type="radio" name="c" value="Titanic"> ' + quest3.a[j] + '</div>');*/
 
 
-        //                 //$questionsDiv.append($newQuestionsDiv);
-
-        //         };
-        // };
-        /*.append(quest1.qa1() + '<br><br>' + quest2.qa2() + '<br><br>' + quest3.qa3());
-
-        function qa(object) {
-                
-                for (i = 0; i < 5; i++) {
-                  var newQdiv = '<input type="radio" name="a" value="answer"> ' + this.a[i] + ' ';
-                }
-                return this.q + '<br>' + this.a
+        //My not quite working loop to grade all the questions with one submit
+        /*$($submit).on('click', function grade() {
+        var a0 = document.getElementsByName('q0');
+          for (var i = 0; i < a0.length; i++) {
+        if (a0[i].checked) console.log(a0[i]); {
+                if (a0[i].value == "Hugo Weaving") {
+                        $right++;
+                        $('#right').html('Correct: ' + $right);
+                } else {
+                        $wrong++;
+                        $('#wrong').html('Incorrect: ' + $wrong);
+                        };
         };
+        };
+        var a1 = document.getElementsByName('q1');
+          for (var j = 0; j < a1.length; j++) {
+        if (a1[j].checked) {
+                if (a1[j].value == "Predator") {
+                        $right++;
+                        $('#right').html('Correct: ' + $right);
+                } else {
+                        $wrong++;
+                        $('#wrong').html('Incorrect: ' + $wrong);
+                        };
+        };
+        };
+        var a2 = document.getElementsByName('q2');
+        for (var l = 0; l < a2.length; l++) {
+        if (a2[l].checked) {
+                if (a2[l].value == "Titanic") {
+                        $right++;
+                        $('#right').html('Correct: ' + $right);
+                } else {
+                        $wrong++;
+                        $('#wrong').html('Incorrect: ' + $wrong);
+                        };
+        };
+        };
+        });*/
 
-        qa(quest1);*/
 
 
-        /*ar $questionsDiv = $('#questions');
-        for (j = 0; j < quest1.a.length; j++) {
-          var $newQuestionsDiv = 
-            $('<div>' + quest1.qa() + '<br><input type="radio" name="a" value="Hugo Weaving"> ' + quest1.a[j] + '<br>' + quest2.qa() + '<br><input type="radio" name="b" value="Predator"> ' + quest2.a[j] + '<br>' + 
-            quest3.qa() + '<br><input type="radio" name="c" value="Titanic"> ' + quest3.a[j] + '</div>');
-            $questionsDiv.append($newQuestionsDiv);
-        };*/
-
-        /*for (j = 0; j < quest1.a.length; j++) {
-                console.log(j);
-                var $newQuestDiv = '<input type="radio" name="a" value="Hugo Weaving"> ' + quest1.a[j] + ' ';
-                console.log($newQuestDiv);
-                $newQuestionsDiv.append($newQuestDiv);
-                console.log($newQuestionsDiv);
-        };*/
-
-});
+        });
