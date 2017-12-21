64a88baeb37f3efae8395004a93f3a8395b5595e
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-12-21 00:01:32 -0500

    updating pseudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -86,7 +86,7 @@ $(document).ready(function () {
 
         var $submit = ('#submit-btn');
         
-        var $button2 = $('<br><br><input>') // name="submit" type="button" value="submit">');
+        var $button2 = $('<br><br><input>')
                 .attr('name', 'submit')
                 .attr('type', 'button')
                 .attr('value', 'submit');
